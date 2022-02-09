b24717548188420d49383d13cdab2cdaefd075ba
josevalterna@gmail.com <yo-mismo>
2022-02-09 14:49:46 +0100

    Anyadida pantalla principal, falta Sprite de soldados y dibujarlos en draw_window

--- game.py
+++ game.py
@@ -3,8 +3,15 @@ from Team import *
 
 HEIGHT, WIDTH = 600, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("AIBG")
+FLOOR = (209, 196, 75)
+team0 = [Soldier.mock_soldier(0)]
 
-team1 = [Soldier.mock_soldier()]
+
+def draw_window():
+    WIN.fill(FLOOR)
+    rect = pygame.draw.rect(WIN, (255, 0, 0), )
+    pygame.display.update()
 
 
 def main():
@@ -13,6 +20,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        draw_window()
+    pygame.quit()
 
 
 if __name__ == "__main__":
