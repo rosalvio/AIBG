8421c09f600aaa944041333312f1f5ad84eea975
rosalvio <52211985+rosalvio@users.noreply.github.com>
2022-03-09 21:11:12 +0100

    Server funcional con reqparse

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-
+import json
+from Soldier import Soldier
+from Team import Team
 
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
+parser.add_argument('soldier', help='Pasa su equipo.')
 
 
 class AddSoldier(Resource):
 
-    parser.add_argument('soldier', help='Pasa su equipo.')
-
     def post(self):
-        json_req = request.get_json()
-        return {'You sent ': json_req}
+        args = parser.parse_args()
+        print(args)
+        res = {'team': args['soldier']}
+        return res, 201
 
 
 api.add_resource(AddSoldier, '/')
